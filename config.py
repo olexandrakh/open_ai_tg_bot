0e79e677a23a3090c5e8f0e2fad79f71dd17991e
olexandrakh <olexandra.khodakovska@gmail.com>
2025-12-19 22:51:53 +0200

    Added .env, .env.example, config.py files

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+CHATGPT_TOKEN = os.getenv("CHATGPT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+print(CHATGPT_TOKEN)
+print(BOT_TOKEN)
+
+
