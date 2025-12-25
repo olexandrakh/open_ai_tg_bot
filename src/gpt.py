1dbad874b29a707f64b08ade0233d639e4da458e
olexandrakh <olexandra.khodakovska@gmail.com>
2025-12-25 21:47:31 +0200

    feat(gpt): add GPT command with text and image response

--- src/gpt.py
+++ src/gpt.py
@@ -1,92 +1,39 @@
-import logging
-from random import choice
-
-from telegram import Update
-from telegram.ext import ContextTypes
-
-from config import CHATGPT_TOKEN
-from gpt import ChatGPTService
-from utils import (send_image, send_text, load_message, show_main_menu, load_prompt, send_text_buttons)
-
-chatgpt_service = ChatGPTService(CHATGPT_TOKEN)
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-logger = logging.getLogger(__name__)
+from openai import OpenAI
+import httpx
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await send_image(update, context, "start")
-    await send_text(update, context, load_message("start"))
-    await show_main_menu(
-        update,
-        context,
-        {
-            'start': 'Головне меню',
-            'random': 'Дізнатися випадковий факт',
-            'gpt': 'Запитати ChatGPT',
-            'talk': 'Діалог з відомою особистістю',
-        }
-    )
+class ChatGPTService:
+    client: OpenAI = None
+    message_list: list = None
 
-
-async def random(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await send_image(update, context, "random")
-    message_to_delete = await send_text(update, context, "Шукаю випадковий факт ...")
-    try:
-        prompt = load_prompt("random")
-        fact = await chatgpt_service.send_question(
-            prompt_text=prompt,
-            message_text="Розкажи про випадковий факт"
+    def __init__(self, token):
+        self.client = OpenAI(
+            http_client=httpx.Client(proxy="http://18.199.183.77:49232"),
+            api_key=token
         )
-        buttons = {
-            'random': 'Хочу ще один факт',
-            'start': 'Закінчити'
-        }
-        await send_text_buttons(update, context, fact, buttons)
-    except Exception as e:
-        logger.error(f"Помилка в обробнику /random: {e}")
-        await send_text(update, context, "Помилка при отриманні випадкового факту.")
-    finally:
-        await context.bot.delete_message(
-            chat_id=update.effective_chat.id,
-            message_id=message_to_delete.message_id
+        self.message_list = []
+
+    async def send_message_list(self) -> str:
+        completion = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=self.message_list,
+            max_tokens=3000,
+            temperature=0.9
         )
+        message = completion.choices[0].message
+        self.message_list.append(message)
+        return message.content
 
+    def set_prompt(self, prompt_text: str) -> None:
+        self.message_list.clear()
+        self.message_list.append({"role": "system", "content": prompt_text})
 
-async def random_button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    query = update.callback_query
-    await query.answer()
-    data = query.data
-    if data == 'random':
-        await random(update, context)
-    elif data == 'start':
-        await start(update, context)
+    async def add_message(self, message_text: str) -> str:
+        self.message_list.append({"role": "user", "content": message_text})
+        return await self.send_message_list()
 
-
-async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    context.user_data.clear()
-    await send_image(update, context, "gpt")
-    chatgpt_service.set_prompt(load_prompt("gpt"))
-    await send_text(update, context, "Задайте питання ...")
-    context.user_data["conversation_state"] = "gpt"
-
-
-async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message_text = update.message.text
-    conversation_state = context.user_data.get("conversation_state")
-    if conversation_state == "gpt":
-        waiting_message = await send_text(update, context, "...")
-        try:
-            response = await chatgpt_service.add_message(message_text)
-            await send_text(update, context, response)
-        except Exception as e:
-            logger.error(f"Помилка при отриманні відповіді від ChatGPT: {e}")
-            await send_text(update, context, "Виникла помилка при обробці вашого повідомлення.")
-        finally:
-            await context.bot.delete_message(
-                chat_id=update.effective_chat.id,
-                message_id=waiting_message.message_id
-            )
+    async def send_question(self, prompt_text: str, message_text: str) -> str:
+        self.message_list.clear()
+        self.message_list.append({"role": "system", "content": prompt_text})
+        self.message_list.append({"role": "user", "content": message_text})
+        return await self.send_message_list()
--- src/handlers.py
+++ src/handlers.py
@@ -1,10 +1,12 @@
 import logging
+from random import choice
 
 from telegram import Update
 from telegram.ext import ContextTypes
-from utils import (send_image, send_text, load_message, show_main_menu, load_prompt, send_text_buttons)
-from gpt import ChatGPTService
+
 from config import CHATGPT_TOKEN
+from gpt import ChatGPTService
+from utils import (send_image, send_text, load_message, show_main_menu, load_prompt, send_text_buttons)
 
 chatgpt_service = ChatGPTService(CHATGPT_TOKEN)
 
@@ -12,24 +14,24 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
-
 logger = logging.getLogger(__name__)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await send_image(update, context, 'start')
-    await send_text(update, context, load_message('start'))
+    await send_image(update, context, "start")
+    await send_text(update, context, load_message("start"))
     await show_main_menu(
         update,
         context,
-    {
+        {
             'start': 'Головне меню',
             'random': 'Дізнатися випадковий факт',
             'gpt': 'Запитати ChatGPT',
             'talk': 'Діалог з відомою особистістю',
-            'quiz': 'Перевірити свої знання'
         }
     )
+
+
 async def random(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await send_image(update, context, "random")
     message_to_delete = await send_text(update, context, "Шукаю випадковий факт ...")
@@ -62,3 +64,29 @@ async def random_button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
         await random(update, context)
     elif data == 'start':
         await start(update, context)
+
+
+async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data.clear()
+    await send_image(update, context, "gpt")
+    chatgpt_service.set_prompt(load_prompt("gpt"))
+    await send_text(update, context, "Задайте питання ...")
+    context.user_data["conversation_state"] = "gpt"
+
+
+async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_text = update.message.text
+    conversation_state = context.user_data.get("conversation_state")
+    if conversation_state == "gpt":
+        waiting_message = await send_text(update, context, "...")
+        try:
+            response = await chatgpt_service.add_message(message_text)
+            await send_text(update, context, response)
+        except Exception as e:
+            logger.error(f"Помилка при отриманні відповіді від ChatGPT: {e}")
+            await send_text(update, context, "Виникла помилка при обробці вашого повідомлення.")
+        finally:
+            await context.bot.delete_message(
+                chat_id=update.effective_chat.id,
+                message_id=waiting_message.message_id
+            )
